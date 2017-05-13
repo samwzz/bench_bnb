9606cedc3ad523a6e1c47991a063cf1f2f061be2
Sam Wang <sam.zz.wang@gmail.com>
2017-05-13 16:04:11 -0700

    Bootstrap current user, add preloadedState

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
-import { signin, signup } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const store = configureStore();
-    window.store = store;
-    window.signin = signin;
-    window.signup = signup;
-    const root = document.getElementById('root');
-    ReactDOM.render(<Root store={ store }/>, root);
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser}};
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={ store }/>, root);
 });
--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -2,7 +2,7 @@ export const signup = (user) =>(
   $.ajax({
     method: 'post',
      url: '/api/users/',
-     data: user
+     data: { user: user }
   })
 );
 
@@ -10,7 +10,7 @@ export const signin = (user) =>(
   $.ajax({
     method: 'post',
      url: '/api/session/',
-     data: user
+     data: { user: user }
   })
 );
 
