34c52f36b99b7e63817cf829240750f0c3284336
Sam Wang <sam.zz.wang@gmail.com>
2017-05-13 02:08:42 -0700

    Add App and Root components, update entry point

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as APIUtil from './util/session_api_util';
+import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+    const store = configureStore();
     const root = document.getElementById('root');
-    window.store = configureStore();
-    window.signin = APIUtil.signin;
-    window.signup = APIUtil.signup;
-    window.signout = APIUtil.signout;
-
-    ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+    ReactDOM.render(<Root store={ store }/>, root);
 });
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from '../actons/session_actions';
+import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from '../actions/session_actions';
 import { merge } from 'lodash';
 
 const nullUser = {
