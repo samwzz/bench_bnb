bc85c7b671e0ff4777f349b7f5de2697f1de2e27
Sam Wang <sam.zz.wang@gmail.com>
2017-05-15 11:13:28 -0700

    Add benchesReducer and update RootReducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
+import benchesReducer from './benches_reducer';
 
 export default combineReducers({
-  session: SessionReducer
+  session: SessionReducer,
+  benches: benchesReducer
 });
